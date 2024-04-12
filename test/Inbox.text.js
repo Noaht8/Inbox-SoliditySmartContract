1a527f85d4558d4cce84950c9ac4fc5dc9cf4361
Noaht8 <noahtsegay8@gmail.com>
2024-04-12 09:44:21 +0300

    Update Deploy.js

--- test/Inbox.text.js
+++ test/Inbox.text.js
@@ -9,22 +9,32 @@ let accounts;
 let inbox;
 
 beforeEach(async () => {
-    // Get a list of all accounts
-    accounts = await web3.eth.getAccounts();
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
 
-    // Use one of those accounts to deploy the contract
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ["Hi there!"] })
-        .send({ from: accounts[0], gas: "1000000" });
-        inbox.setProvider(provider);
-    });
-
-describe("Inbox", () => {
-    it("deploys a contract", () => {
-        assert.ok(inbox.options.address);
-    });
+  // Use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .send({ from: accounts[0], gas: "1000000" });
+  inbox.setProvider(provider);
 });
 
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Hi there!");
+  });
+
+  it("can set the message", async () => {
+    await inbox.methods.setMessage("How are you?").send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "How are you?");
+  });
+});
 
 // class Car {
 //   park() {
